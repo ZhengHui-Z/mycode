d1a7fc5156c3dca5ea8dcbc871ff1d77018a5328
ZhengHui-Z <344237028@qq.com>
2017-10-28 19:50:59 +0800

    结构体

--- data_structure/test.cpp
+++ data_structure/test.cpp
@@ -8,7 +8,7 @@ using namespace std;
 typedef struct Node
 {
     int data;
-    //struct Node *next;
+    struct Node *next;
 }Node;
 typedef struct Node *LinkList;
 int main(int argc, char const *argv[])
@@ -21,13 +21,19 @@ int main(int argc, char const *argv[])
 	cout<<"test = "<<test<<endl;
 	cout<<"&i = "<<&i<<endl;*/
 	///结构体指针
-	LinkList a;
+	LinkList a,b;
 	a = (LinkList)malloc(sizeof(Node));
+	b = (LinkList)malloc(sizeof(Node));
 	a->data = 123;
+	b->data = 234;
+	a->next = b;
 	cout<<"addr = "<< (int *)a <<endl;
 	cout<<"data = "<< a->data <<endl;
+	cout<<"sizeof = "<<sizeof(Node)<<endl;
+	cout<<"a->next->data = "<<a->next->data<<endl;
 
 	free(a);
+	free(b);
 
 	return 0;
 }
